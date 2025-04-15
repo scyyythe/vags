2edd486b02217041ce307f4c6262677b2674027f
JaiAnoba <kaeanver@gmail.com>
2025-04-15 23:10:58 +0800

    popular artist adjustment

--- front/src/pages/PopularArtists.tsx
+++ front/src/pages/PopularArtists.tsx
@@ -39,7 +39,7 @@ const PopularArtists = () => {
         </motion.h2>
 
         <motion.div 
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10"
+          className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10"
           variants={container}
           initial="hidden"
           whileInView="show"
