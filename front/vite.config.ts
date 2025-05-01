fe6210a0d859b53abaf3ae9f33ed6bace37b7619
scyyythe <caneteangel187@gmail.com>
2025-05-01 18:48:29 +0800

    modified ngrok, incomplete vercel config, deployed only the front

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["56aa-61-245-19-236.ngrok-free.app"],
+    allowedHosts: ["0794-61-245-19-236.ngrok-free.app"],
     proxy: {
       "/api": "http://127.0.0.1:8000",
     },
