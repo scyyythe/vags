05a0e7c539af7775c4141c96db363e9f6c058a89
JaiAnoba <kaeanver@gmail.com>
2025-05-04 00:37:19 +0800

    layout for user profile

---
+++ front/src/components/user_dashboard/user_profile/UserProfile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import Header from "@/components/user_dashboard/navbar/Header";
+import ProfileHeader from "@/components/user_dashboard/user_profile/ProfileHeader";
+import ProfileTabs from "@/components/user_dashboard/user_profile/ProfileTabs";
+import ArtGrid from "@/components/user_dashboard/user_profile/ArtGrid";
+
+const Index = () => {
+    const { artistName } = useParams();
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Header />
+        <div className="container mx-auto mt-20 px-4 pt-4 pb-12">
+            <ProfileHeader 
+            bannerImage="/lovable-uploads/91de0ca2-cd8a-42fb-8dbe-bad642960399.png"
+            profileImage="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3"
+            name="Angel Canete"
+            followers={99}
+            following={50}
+            items={15}
+            />
+            <ProfileTabs />
+            <ArtGrid />
+        </div>
+        </div>
+    );
+};
+
+export default Index;
