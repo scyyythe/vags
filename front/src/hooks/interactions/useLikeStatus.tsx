a18b1e83dcb3bdf936844174d84a7c861e931456
scyyythe <caneteangel187@gmail.com>
2025-05-05 01:44:50 +0800

    organized in folder

--- front/src/hooks/interactions/useLikeStatus.tsx
+++ front/src/hooks/interactions/useLikeStatus.tsx
@@ -7,11 +7,10 @@ const fetchLikeStatus = async (id: string) => {
 };
 
 const useLikeStatus = (id: string) => {
-  // Updated to use valid configuration options
   return useQuery({
-    queryKey: ["likeStatus", id], // Query key
-    queryFn: () => fetchLikeStatus(id), // Query function
-    staleTime: 1000 * 60 * 5, // Use staleTime if needed for data freshness (5 minutes)
+    queryKey: ["likeStatus", id],
+    queryFn: () => fetchLikeStatus(id),
+    staleTime: 1000 * 60 * 5,
   });
 };
 
