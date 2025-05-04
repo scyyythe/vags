5ba43abe085f9ac34ce88a55a006c76597912586
JaiAnoba <kaeanver@gmail.com>
2025-05-04 18:21:42 +0800

    fecthed artworks and display in created tab section

--- front/src/components/user_dashboard/user_profile/tabs/CreatedTab.tsx
+++ front/src/components/user_dashboard/user_profile/tabs/CreatedTab.tsx
@@ -9,7 +9,7 @@ const CreatedTab = () => {
   }
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-6">
       {artworks.map((art) => (
         <ArtCard
           key={art.id}
