d60224cfede3dfa1176ea4581a6cdfb8c6ac078c
JaiAnoba <kaeanver@gmail.com>
2025-05-04 12:57:59 +0800

    adjustments in bidding dashboard

--- front/src/components/user_dashboard/Bidding/navbar/Components.tsx
+++ front/src/components/user_dashboard/Bidding/navbar/Components.tsx
@@ -33,27 +33,25 @@ const Components = () => {
         {/* Right Section */}
         <div
           className={`flex items-center ${
-            isMobile ? "w-full justify-start space-x-2" : "space-x-2"
+            isMobile ? "w-full justify-start space-x-2" : "space-x-4"
           } mt-0 sm:mt-0`}
         >
-          <div className="relative flex-grow max-w-full sm:max-w-sm">
+          <div className="relative w-full sm:w-sm md:w-xl">
             <input
               type="text"
               placeholder="Browse now"
-              className={cn("pl-4 pr-10 py-2 border rounded-full w-full", isMobile ? "text-[10px]" : "text-xs"
+              className={cn("pl-4 pr-10 py-[6px] border border-gray-400 rounded-full w-full", isMobile ? "text-[10px]" : "text-[11px]"
               )}
             />
-            <Search className="absolute right-3 top-2.5 w-3 h-3 text-gray-400" />
+            <Search className="absolute right-3 top-2.5 w-[10px] h-[10px] text-gray-400" />
           </div>
-          <Button
-            variant="outline"
-            size="sm"
-            className={cn("flex items-center rounded-full whitespace-nowrap", isMobile ? "text-[10px]" : "text-xs"
+          <button
+            className={cn("flex items-center px-3 py-[6px] gap-2 border border-gray-400 rounded-full whitespace-nowrap", isMobile ? "text-[10px]" : "text-[11px]"
              )}
              >
-            <i className={cn("bx bx-sort-alt-2 -mr-1", isMobile ? "text-xs" : "text-sm")}></i>
+            <i className={cn("bx bx-sort-alt-2 -mr-1", isMobile ? "text-[10px]" : "text-[11px]")}></i>
             Sort
-          </Button>
+          </button>
         </div>
       </div>
     </section>
