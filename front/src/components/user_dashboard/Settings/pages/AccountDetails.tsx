05ca4ffb4dcb8a4ced195b6eec867dd2dc838170
JaiAnoba <kaeanver@gmail.com>
2025-05-08 00:03:50 +0800

    edit profile and account details

--- front/src/components/user_dashboard/Settings/pages/AccountDetails.tsx
+++ front/src/components/user_dashboard/Settings/pages/AccountDetails.tsx
@@ -35,7 +35,7 @@ const AccountDetails = () => {
 
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-6">Account Information</h2>
+      <h2 className="text-sm font-bold mb-6">Account Information</h2>
       
       <div className="bg-white border border-gray-200 rounded-lg p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-4">
@@ -84,31 +84,33 @@ const AccountDetails = () => {
       </div>
       
       <div className="mt-12">
-        <h2 className="text-2xl font-bold mb-6">Deactivation and Deletion</h2>
+        <h2 className="text-sm font-bold mb-6">Deactivation and Deletion</h2>
         
         <div className="bg-white border border-gray-200 rounded-lg p-6">
-          <div className="mb-8">
-            <h3 className="text-lg font-medium mb-2">Deactivate account</h3>
-            <p className="text-gray-600 mb-4 text-sm">
-              Temporarily hide your profile, uploaded artworks, and activity within the gallery. While
-              deactivated, your content won't be visible to other users, but your data will be saved
-              and can be restored at any time by reactivating your account.
-            </p>
-            <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 py-2 px-4 rounded">
-              Deactivate Account
-            </button>
-          </div>
+            <h3 className="text-xs font-semibold mb-2">Deactivate account</h3>
+            <div className="grid grid-cols-2 gap-10"> 
+              <p className="text-gray-600 mb-4 text-[11px]">
+                Temporarily hide your profile, uploaded artworks, and activity within the gallery. While
+                deactivated, your content won't be visible to other users, but your data will be saved
+                and can be restored at any time by reactivating your account.
+              </p>
+              <button className="bg-gray-200 font-medium text-[10px] hover:bg-gray-300 text-gray-800 p-none rounded-sm w-32 h-9 whitespace-nowrap">
+                Deactivate Account
+              </button>
+            </div>  
           
           <div>
-            <h3 className="text-lg font-medium mb-2">Delete your data and account</h3>
-            <p className="text-gray-600 mb-4 text-sm">
-              Permanently remove your account from the system, including all uploaded artworks,
-              favorites, exhibition history, and profile details. This action is irreversible and your data
-              cannot be recovered once deleted.
-            </p>
-            <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 py-2 px-4 rounded">
-              Delete Account
-            </button>
+            <h3 className="text-xs font-semibold mb-2">Delete your data and account</h3>
+            <div className="grid grid-cols-2 gap-10">
+              <p className="text-gray-600 mb-4 text-[11px]">
+                Permanently remove your account from the system, including all uploaded artworks,
+                favorites, exhibition history, and profile details. This action is irreversible and your data
+                cannot be recovered once deleted.
+              </p>
+              <button className="bg-gray-200 font-medium text-[10px] hover:bg-gray-300 text-gray-800 rounded-sm w-32 h-9">
+                Delete Account
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- front/src/components/user_dashboard/Settings/pages/EditProfile.tsx
+++ front/src/components/user_dashboard/Settings/pages/EditProfile.tsx
@@ -52,98 +52,85 @@ const EditProfile = () => {
 
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-6">Edit Profile</h2>
+      <h2 className="text-sm font-bold mb-6">Edit Profile</h2>
       
-      <div className="bg-white border border-gray-200 rounded-lg p-6">
-        <div className="mb-8">
-          <p className="text-sm text-gray-500 mb-2">Photo</p>
-          <div className="flex flex-col items-center sm:items-start sm:flex-row gap-4">
-            <img
-              src={formData.photo}
-              alt="Profile"
-              className="w-32 h-32 rounded-full object-cover"
+      <div className="mb-8">
+        <p className="text-xs pl-12 text-gray-500 mb-4">Photo</p>
+        <div className="flex flex-col items-center sm:items-start sm:flex-row gap-4">
+          <img
+            src={formData.photo}
+            alt="Profile"
+            className="w-32 h-32 rounded-full object-cover"
+          />
+          <div className="flex flex-col justify-center relative top-12">
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handlePhotoChange}
+              accept="image/*"
+              className="hidden"
             />
-            <div className="flex flex-col justify-center">
-              <input
-                type="file"
-                ref={fileInputRef}
-                onChange={handlePhotoChange}
-                accept="image/*"
-                className="hidden"
-              />
-              <Button
-                variant="secondary"
-                onClick={triggerFileInput}
-                className="bg-gray-200 hover:bg-gray-300 text-gray-800"
-              >
-                Change
-              </Button>
-            </div>
+            <button
+              onClick={triggerFileInput}
+              className="text-[10px] font-medium py-2 px-3 rounded-sm bg-gray-200 hover:bg-gray-300 text-gray-800"
+            >
+              Change
+            </button>
           </div>
         </div>
-        
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div>
-            <label className="block text-sm text-gray-500 mb-2">Full name</label>
-            <div className="relative">
-              <Input
-                value={formData.fullName}
-                onChange={(e) => handleChange("fullName", e.target.value)}
-                className="w-full pr-10"
-              />
-              <button
-                className="absolute right-2 top-2 text-gray-400 hover:text-gray-600"
-                onClick={() => {}}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+      </div>  
+
+      <div className="grid grid-cols-2 gap-4">
+        <div className="bg-white border border-gray-200 rounded-md px-4 py-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div>
+              <label className="block text-[10px] text-gray-500 pl-3">Full name</label>
+              <div className="relative">
+                <Input
+                  value={formData.fullName}
+                  onChange={(e) => handleChange("fullName", e.target.value)}
+                  className="w-full font-semibold -mb-2 p-none border-none focus:ring-0 shadow-none"
+                  style={{
+                    border: "none",
+                    fontSize: "12px",
+                    boxShadow: "none",
+                    outline: "none",
+                  }}
+                />
+                <button
+                  className="absolute right-2 top-2 text-gray-400 hover:text-gray-600"
+                  onClick={() => {}}
                 >
-                  <path d="M17 3a2.85 2.85 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-                  <path d="m15 5 4 4" />
-                </svg>
-              </button>
-            </div>
-          </div>
-          
+                </button>
+              </div>
+            </div> 
+          </div> 
+        </div>     
+
+        <div className="bg-white border border-gray-200 rounded-md px-4 py-4"> 
           <div>
-            <label className="block text-sm text-gray-500 mb-2">Username</label>
+            <label className="block text-[10px] text-gray-500 pl-3">Username</label>
             <div className="relative">
               <Input
                 value={formData.username}
                 onChange={(e) => handleChange("username", e.target.value)}
-                className="w-full pr-10"
+                className="w-full font-semibold -mb-2 p-none border-none focus:ring-0 shadow-none"
+                style={{
+                  border: "none",
+                  fontSize: "12px",
+                  boxShadow: "none",
+                  outline: "none",
+                }}
               />
               <button
                 className="absolute right-2 top-2 text-gray-400 hover:text-gray-600"
                 onClick={() => {}}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M17 3a2.85 2.85 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-                  <path d="m15 5 4 4" />
-                </svg>
               </button>
             </div>
           </div>
         </div>
-      </div>
+      </div>  
       
       <ActionButtons
         hasChanges={hasChanges()}
