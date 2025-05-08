d362d7534f91b44d7c5e6a4d77da949b468a1cf7
scyyythe <caneteangel187@gmail.com>
2025-05-09 01:40:27 +0800

    protected route & decoding auth

--- front/src/components/user_dashboard/navbar/Header.tsx
+++ front/src/components/user_dashboard/navbar/Header.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useState, useRef, useEffect } from "react";
 import ProfileDropdown from "../local_components/ProfileDropdown";
+import { getLoggedInUserId } from "@/auth/decode";
 
 const Header = () => {
   const location = useLocation();
@@ -14,6 +15,11 @@ const Header = () => {
   const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
   const avatarRef = useRef<HTMLDivElement>(null);
 
+  const userId = getLoggedInUserId();
+
+  if (!userId) {
+    return <p>No user found</p>;
+  }
   const toggleProfileDropdown = () => {
     setIsProfileDropdownOpen(!isProfileDropdownOpen);
   };
@@ -34,33 +40,25 @@ const Header = () => {
           <div className="text-xs flex flex-col items-center md:flex-row md:space-x-16">
             <NavLink
               to="/explore"
-              className={({ isActive }) =>
-                `nav-link ${isActive ? "font-bold" : ""} block py-2 px-4 md:inline-block`
-              }
+              className={({ isActive }) => `nav-link ${isActive ? "font-bold" : ""} block py-2 px-4 md:inline-block`}
             >
               Explore
             </NavLink>
             <NavLink
               to="/exhibits"
-              className={({ isActive }) =>
-                `nav-link ${isActive ? "font-bold" : ""} block py-2 px-4 md:inline-block`
-              }
+              className={({ isActive }) => `nav-link ${isActive ? "font-bold" : ""} block py-2 px-4 md:inline-block`}
             >
               Exhibits
             </NavLink>
             <NavLink
               to="/bidding"
-              className={({ isActive }) =>
-                `nav-link ${isActive ? "font-bold" : ""} block py-2 px-4 md:inline-block`
-              }
+              className={({ isActive }) => `nav-link ${isActive ? "font-bold" : ""} block py-2 px-4 md:inline-block`}
             >
               Bidding
             </NavLink>
             <NavLink
               to="/marketplace"
-              className={({ isActive }) =>
-                `nav-link ${isActive ? "font-bold" : ""} block py-2 px-4 md:inline-block`
-              }
+              className={({ isActive }) => `nav-link ${isActive ? "font-bold" : ""} block py-2 px-4 md:inline-block`}
             >
               Marketplace
             </NavLink>
@@ -77,7 +75,7 @@ const Header = () => {
             <Menu className="h-5 w-5 text-black" />
           </button>
           {/* Search Icon and Sliding Search Bar (Responsive) */}
-          <div className="relative flex items-center"> 
+          <div className="relative flex items-center">
             <button
               onClick={() => setIsSearchVisible(!isSearchVisible)}
               className="p-2 hover:bg-gray-100 rounded-full transition-colors"
@@ -98,10 +96,7 @@ const Header = () => {
                 placeholder="Search..."
                 className="flex-1 px-4 border-0 outline-none text-[11px] bg-transparent"
               />
-              <button
-                onClick={() => setIsSearchVisible(false)}
-                className="p-2"
-              >
+              <button onClick={() => setIsSearchVisible(false)} className="p-2">
                 <X className="h-4 w-4 text-gray-600 -ml-2" />
               </button>
             </div>
@@ -127,20 +122,14 @@ const Header = () => {
 
           {/* User Avatar */}
           <div className="relative" ref={avatarRef}>
-            <Avatar 
-              className="h-8 w-8 border cursor-pointer" 
-              onClick={toggleProfileDropdown}
-            >
-              <Link to="/userprofile/">
+            <Avatar className="h-8 w-8 border cursor-pointer" onClick={toggleProfileDropdown}>
+              <Link to={`/userprofile/${userId}`}>
                 <AvatarFallback>
                   <img src="/pics/1.jpg" alt="JAI" />
                 </AvatarFallback>
               </Link>
             </Avatar>
-            <ProfileDropdown 
-              isOpen={isProfileDropdownOpen} 
-              onClose={() => setIsProfileDropdownOpen(false)} 
-            />
+            <ProfileDropdown isOpen={isProfileDropdownOpen} onClose={() => setIsProfileDropdownOpen(false)} />
           </div>
         </div>
       </div>
