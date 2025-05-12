c2d47eabf8b01c99b7e13685193ebacf20045a65
scyyythe <caneteangel187@gmail.com>
2025-05-12 22:11:06 +0800

    unfollow follow tempo

--- backend/api/serializers/interaction_s/follow.py
+++ backend/api/serializers/interaction_s/follow.py
@@ -4,14 +4,11 @@ from api.models.interaction_model.follows import Follower
 from api.serializers.user_s.users_serializers import UserSerializer
 
 class FollowSerializer(serializers.Serializer):
-    follower=UserSerializer(read_only=True)
-    following=UserSerializer(read_only=True)
-    followed_at=serializers.DateTimeField(read_only=True)
+    follower = UserSerializer(read_only=True)
+    following = UserSerializer(read_only=True)
+    followed_at = serializers.DateTimeField(read_only=True)
 
-    def create(self,validated_data):
-        follower = validated_data.get('follower')
-        following = validated_data.get('following')
-        
-        follow_instance = Follower.objects.create(follower=follower, following=following)
-        return follow_instance
+
+
+    
 
