49ddda62809e2f91e096e835239cf6b89a8a9b76
scyyythe <caneteangel187@gmail.com>
2025-05-14 00:06:23 +0800

    create auction view

--- backend/api/routes/auction_routes.py
+++ backend/api/routes/auction_routes.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from api.views.artwork_views.bid_views import AuctionListView, CreateAuctionView, ActiveAuctionsView, CloseAuctionView
+from api.views.artwork_views.bid_views import AuctionCreateView,AuctionListView, ActiveAuctionsView, CloseAuctionView
 
 auction_urlpatterns = [
     path('auction/', AuctionListView.as_view(), name='auction_list'),
-    path('auction/create/', CreateAuctionView.as_view(), name='create_auction'),
+    path('auction/create/', AuctionCreateView.as_view(), name='create_auction'),
     path('auction/active/', ActiveAuctionsView.as_view(), name='active_auctions'),
     path('auction/close/<str:artwork_id>/', CloseAuctionView.as_view(), name='close_auction'),
 ]
