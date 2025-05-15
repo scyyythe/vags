d8c44c80c94babf3bedeaf82f49dea0a21348cd3
scyyythe <caneteangel187@gmail.com>
2025-05-15 22:30:15 +0800

    retreived on bid tab

--- backend/api/routes/auction_routes.py
+++ backend/api/routes/auction_routes.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from api.views.artwork_views.bid_views import AuctionCreateView,AuctionListView, ActiveAuctionsView, CloseAuctionView,AuctionDetailView
+from api.views.artwork_views.bid_views import AuctionCreateView,AuctionListView, ActiveAuctionsView, CloseAuctionView,AuctionDetailView,MyAuctionListView
 
 
 auction_urlpatterns = [
     path('auction/create/', AuctionCreateView.as_view(), name='create_auction'),
     path('auction/', AuctionListView.as_view(), name='auction_list'),  
+    path('auction/my/', MyAuctionListView.as_view(), name='auction_my_list'),  
     path('auction/<str:auction_id>/', AuctionDetailView.as_view(), name='auction_detail'),
     path('auction/active/', ActiveAuctionsView.as_view(), name='active_auctions'),
     path('auction/close/<str:artwork_id>/', CloseAuctionView.as_view(), name='close_auction'),
