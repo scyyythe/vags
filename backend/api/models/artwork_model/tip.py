8062fec5cb867945525ad87e50c958fcbbcc8e94
scyyythe <caneteangel187@gmail.com>
2025-05-20 00:09:07 +0800

    connected to paypal (not yet for the backend)

--- backend/api/models/artwork_model/tip.py
+++ backend/api/models/artwork_model/tip.py
@@ -1,11 +1,15 @@
-from mongoengine import Document, ReferenceField, FloatField, DateTimeField
+from mongoengine import Document,ReferenceField,FloatField,DateTimeField,StringField
 from datetime import datetime
 from ..user_model.users import User
 
 class Tip(Document):
-    sender = ReferenceField(User, required=True, reverse_delete_rule=2)  
-    receiver = ReferenceField(User, required=True, reverse_delete_rule=2)  
-    amount = FloatField(required=True, min_value=0.1)  
-    timestamp = DateTimeField(default=datetime.utcnow) 
+    sender = ReferenceField(User, required=True, reverse_delete_rule=2)
+    receiver = ReferenceField(User, required=True, reverse_delete_rule=2)
+    amount = FloatField(required=True, min_value=0.1)
+    currency = StringField(default="PHP")  
+    payment_method = StringField(required=True, choices=["PayPal", "Stripe", "GCash"])
+    payment_status = StringField(default="Pending", choices=["Pending", "Completed", "Failed"])
+    transaction_id = StringField()  
+    timestamp = DateTimeField(default=datetime.utcnow)
 
-    meta = {'collection': 'donations'} 
+    meta = {'collection': 'donations'}
