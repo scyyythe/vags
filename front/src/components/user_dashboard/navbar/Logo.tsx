b4718cd98dc827177e7bf9ca10c881b05c6ccdd0
JaiAnoba <kaeanver@gmail.com>
2025-06-08 12:39:18 +0800

    not sure for the logo

--- front/src/components/user_dashboard/navbar/Logo.tsx
+++ front/src/components/user_dashboard/navbar/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to="/explore" className="flex">
-      <img src="/pics/logo.png" alt="logo" className=" w-10 h-10 " />
+      <img src="/pics/Wx.png" alt="logo" className=" w-11 h-[30px] " />
     </Link>
   );
 };
