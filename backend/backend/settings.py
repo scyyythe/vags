d2621b5578e7f1688c1ce4c2731a969222277cc7
scyyythe <caneteangel187@gmail.com>
2025-06-16 23:28:36 +0800

    modified

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -180,9 +180,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080", 
