d5f5ae1c14ae06e12bc501c4c503c74b00d54b53
JaiAnoba <kaeanver@gmail.com>
2025-07-10 15:23:18 +0800

    need more fixing for the settings width

--- front/src/components/user_dashboard/Settings/pages/Settings.tsx
+++ front/src/components/user_dashboard/Settings/pages/Settings.tsx
@@ -17,18 +17,23 @@ const Settings = () => {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Header />
-      
-      <main className="flex-grow max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10 mt-10">
-        <h1 className="text-lg font-bold mb-8">Profile Settings</h1>
-        
+
+      <main className="flex-grow w-full px-4 sm:px-6 lg:px-8 py-6 md:py-10 mt-10">
+        <h1 className="text-lg font-bold mb-8 text-left">Profile Settings</h1>
+
         <ProfileHeader
           name="Angel Canete"
           email="angelcanete5@gmail.com"
           imageUrl="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3"
         />
-        
-        <SettingsTabs />
-        <Outlet />
+
+        <div className="mt-6">
+          <SettingsTabs />
+        </div>
+
+        <div className="mt-6">
+          <Outlet />
+        </div>
       </main>
 
       <Footer />
