14eb2cdb695107d7dcbd7fbe44b90a4ac63c868a
Jperitos <jeralynperitos@gmail.com>
2025-07-24 00:03:49 +0800

    modified

--- backend/api/routes/purchase_routes.py
+++ backend/api/routes/purchase_routes.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views.purchase_view import PurchaseArtworkView
+from api.views.purchase_view.purchase_view import PurchaseArtworkView
 purchase_urlpatterns = [
 
    path("purchase/", PurchaseArtworkView.as_view(), name="purchase-artwork"),
