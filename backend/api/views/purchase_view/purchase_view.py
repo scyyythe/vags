5cfe1860802b1e37727b97c26a59f3382a43cf95
Jperitos <jeralynperitos@gmail.com>
2025-07-24 00:03:49 +0800

    modified

--- backend/api/views/purchase_view/purchase_view.py
+++ backend/api/views/purchase_view/purchase_view.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from api.serializers.purchase_serializer import PurchaseArtworkSerializer
+from api.serializers.purchase_serializer.purchase_serializer import PurchaseArtworkSerializer
 
 
 class PurchaseArtworkView(APIView):
