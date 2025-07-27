dcded4a1c575ae5d2d0a3dc5e266877d91d5f140
scyyythe <caneteangel187@gmail.com>
2025-07-27 10:55:45 +0800

    modified model purchase artwork default status

--- backend/api/models/purchase_model/order.py
+++ backend/api/models/purchase_model/order.py
@@ -25,7 +25,7 @@ class PurchasedArtwork(Document):
     is_paid = BooleanField(default=False)
     quantity = IntField(default=1)
     total_price = FloatField(required=True)
-    status = StringField(choices=["Pending", "Paid", "Shipped", "Reviewed"], default="Pending")
+    status = StringField(choices=["Pending", "Paid", "Shipped", "Reviewed","Completed"], default="Completed")
     created_at = DateTimeField(default=datetime.utcnow)
     updated_at = DateTimeField(default=datetime.utcnow)
 
