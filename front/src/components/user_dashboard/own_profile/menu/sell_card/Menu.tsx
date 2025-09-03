87e9d255e38d327d7c5a1841f49c12020c1c8696
scyyythe <caneteangel187@gmail.com>
2025-09-03 19:46:57 +0800

    added new hook for modifying sell card but not yet applied

--- front/src/components/user_dashboard/own_profile/menu/sell_card/Menu.tsx
+++ front/src/components/user_dashboard/own_profile/menu/sell_card/Menu.tsx
@@ -74,7 +74,7 @@ const SellMenu: React.FC<SellMenuProps> = ({
   };
 
   const handleEdit = () => {
-    navigate("/sell", { state: { artworkId } });
+    navigate("/sell-update", { state: { artworkId } });
     setIsMoreOptionsOpen(false);
   };
 
