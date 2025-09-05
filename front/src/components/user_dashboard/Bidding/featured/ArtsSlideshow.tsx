b2cd5dd86de59a2308ec320887444ce8096f491a
JaiAnoba <kaeanver@gmail.com>
2025-09-05 14:40:46 +0800

    adjusted header and mobile view in auction slideshow

--- front/src/components/user_dashboard/Bidding/featured/ArtsSlideshow.tsx
+++ front/src/components/user_dashboard/Bidding/featured/ArtsSlideshow.tsx
@@ -3,35 +3,37 @@ import { motion } from "framer-motion";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
 import BidPopup from "../place_bid/BidPopup";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import usePopularAuctions from "@/hooks/auction/featured/usePopularAuctions";
 import CountdownDisplay from "./CountdownDisplay";
 import { Artwork } from "@/hooks/artworks/owner/useMyArtworks";
 import { User } from "@/hooks/users/useUserQuery";
 import FeaturedAuctionSkeleton from "@/components/skeletons/FeaturedAuction";
-import { useNavigate } from "react-router-dom";
+
 interface ArtSlideshowProps {
   artworks: Artwork[];
   autoPlay?: boolean;
   interval?: number;
   user?: User;
 }
+
 const ArtSlideshow = memo(({ artworks, user, autoPlay = true, interval = 4000 }: ArtSlideshowProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const isMobile = useIsMobile();
   const [showBidPopup, setShowBidPopup] = useState(false);
   const [bidArtworkIndex, setBidArtworkIndex] = useState<number | null>(null);
   const { data, isLoading, isError } = usePopularAuctions();
+  const navigate = useNavigate();
 
   const handleBidSubmit = (amount: number) => {
     setShowBidPopup(false);
   };
-  const navigate = useNavigate();
 
   const fadeIn = {
     initial: { opacity: 0 },
     animate: { opacity: 1, transition: { duration: 0.5, ease: "easeOut" } },
   };
+
   const auctions = data ?? [];
 
   useEffect(() => {
@@ -54,7 +56,8 @@ const ArtSlideshow = memo(({ artworks, user, autoPlay = true, interval = 4000 }:
       </div>
     );
   }
-  const formatCurrency = (amount) => {
+
+  const formatCurrency = (amount: any) => {
     if (amount == null) return "No bids";
 
     const num = parseFloat(amount);
@@ -69,80 +72,67 @@ const ArtSlideshow = memo(({ artworks, user, autoPlay = true, interval = 4000 }:
   const selectedArtwork = bidArtworkIndex !== null ? auctions[bidArtworkIndex] : null;
 
   return (
-    <div className={cn("", "flex items-center", isMobile ? "px-16 gap-1" : "px-8 py-8 gap-12")}>
+    <div className={cn("", "items-center", isMobile ? "px-10 gap-1" : "px-8 py-8 gap-12")}>
       {auctions.map((artwork, index) => (
         <div
           key={artwork.id}
           className={cn(
-            "absolute top-0 left-0 w-full h-full flex items-center gap-20 transition-opacity duration-[2500ms] ease-in-out",
+            "absolute top-0 left-0 w-full h-full transition-opacity duration-[2500ms] ease-in-out",
+            isMobile
+              ? "flex flex-col items-center gap-6"
+              : "flex flex-row items-center gap-20", 
             index === currentIndex ? "opacity-100 z-10" : "opacity-0 z-0 pointer-events-none"
           )}
           aria-hidden={index !== currentIndex}
         >
           {/* Left - Artwork Image */}
-          <div className={cn("aspect-square overflow-hidden py-16 pl-28 ml-6", isMobile ? "w-[45%] -mr-8" : "w-[38%]")}>
-            <motion.img
+          <div
+            className={cn(
+              "overflow-hidden pt-1",
+              isMobile ? "pt-4" : "pl-28"
+            )}
+          >
+            <img
               src={artwork.artwork.image_url}
               alt={artwork.artwork.title}
-              className={cn("object-cover rounded-xl shadow-xl", isMobile ? "w-full h-full" : "w-full h-full")}
+              className={cn("object-cover rounded-xl", isMobile ? "w-[350px] h-full" : "w-[350px] h-[350px]")}
               draggable={false}
-              animate={{
-                y: [0, -5, 0],
-              }}
-              transition={{
-                duration: 3,
-                repeat: Infinity,
-                repeatType: "loop",
-                ease: "easeInOut",
-              }}
             />
           </div>
 
           {/* Right - Artwork Info */}
-          <div
-            className={cn("text-white flex flex-col justify-center", isMobile ? "w-[60%] py-8 gap-1" : "w-[50%] gap-4")}
-          >
+          <div className={cn("text-white flex flex-col justify-center", isMobile ? "w-[60%] pb-8 gap-1 -ml-28" : "w-[50%] gap-4")}>
             <h2
               className={cn(
                 "font-semibold",
                 "bg-gradient-to-r from-red-300 via-red-500 to-red-900 bg-clip-text text-transparent",
-                isMobile ? "text-lg mb-1" : "text-3xl"
+                isMobile ? "text-xl mb-1" : "text-3xl"
               )}
             >
               {artwork.artwork.title}
             </h2>
 
-            {/* <p
-              className={cn(
-                "text-black",
-                isMobile ? "text-[9px]" : "text-sm mb-4"
-              )}
-            >
-              {artwork.description}
-            </p> */}
-
             <div className="flex items-center gap-2 mb-3">
-              <span className={cn("text-gray-600", isMobile ? "text-[9px]" : "text-xs")}>Owned By</span>
+              <span className={cn("text-gray-600", isMobile ? "text-[11px]" : "text-xs")}>Owned By</span>
               <div className="flex items-center gap-2 whitespace-nowrap">
                 <Link to={`/userprofile/${artwork.artwork.artist_id}`} className="flex items-center gap-2">
                   <img
                     src={artwork.artwork.profile_picture}
                     alt={artwork.artwork.artist}
-                    className={cn("rounded-full", isMobile ? "w-2 h-2" : "w-4 h-4")}
+                    className={cn("rounded-full", isMobile ? "w-3.5 h-3.5" : "w-4 h-4")}
                   />
-
-                  <span className={cn("text-black font-medium", isMobile ? "text-[9px]" : "text-xs")}>
+                  <span className={cn("text-black font-medium", isMobile ? "text-[11px]" : "text-xs")}>
                     {artwork.artwork.artist}
                   </span>
                 </Link>
               </div>
             </div>
 
-            <div className="bg-white rounded-md px-16 py-7 flex max-w-[475px]">
+            <div className={cn("bg-white rounded-md flex ", isMobile ? "w-[350px] px-6 py-4" : "max-w-[475px] px-16 py-7")}>
               <div className="flex w-full">
-                <div className="flex-1 text-center">
-                  <p className="text-[11px] text-black mb-3">Current Bid</p>
-                  <p className="text-2xl text-black md:text-xl font-semibold whitespace-nowrap">
+                <div className="flex-1 text-center pl-1">
+                  <p className="text-[11px] text-black mb-3 whitespace-nowrap">Current Bid</p>
+                  <p className={cn("text-black font-semibold whitespace-nowrap", isMobile ? "text-lg" : "text-2xl")}>
                     {formatCurrency(artwork.highest_bid?.amount)}
                   </p>
                 </div>
@@ -155,15 +145,15 @@ const ArtSlideshow = memo(({ artworks, user, autoPlay = true, interval = 4000 }:
               </div>
             </div>
 
-            <div className={cn("flex items-center mt-4", isMobile ? "gap-6" : "gap-8")}>
+            <div className={cn("flex items-center mt-4", isMobile ? "gap-6 -mr-[110px]" : "gap-8")}>
               <button
                 onClick={() => {
                   setBidArtworkIndex(index);
                   setShowBidPopup(true);
                 }}
                 className={cn(
-                  "bg-red-800 text-white rounded-full font-medium w-[38%]",
-                  isMobile ? "px-3 py-1 text-[10px]" : "px-8 py-2 text-sm"
+                  "bg-red-800 text-white rounded-full font-medium ",
+                  isMobile ? "w-60 px-3 py-2 text-[11px] " : "w-[38%] px-8 py-2 text-sm"
                 )}
               >
                 Bid Now
@@ -171,8 +161,8 @@ const ArtSlideshow = memo(({ artworks, user, autoPlay = true, interval = 4000 }:
               <button
                 onClick={() => navigate(`/bid/${artwork.id}/`)}
                 className={cn(
-                  "border border-gray-400 text-gray-500 rounded-full font-medium transition w-[38%]",
-                  isMobile ? "px-3 py-1 text-[10px] " : "px-8 py-2 text-sm"
+                  "border border-gray-400 text-gray-500 rounded-full font-medium transition ",
+                  isMobile ? "w-60 px-3 py-2 text-[11px]" : "w-[38%] px-8 py-2 text-sm"
                 )}
               >
                 View item
@@ -196,9 +186,7 @@ const ArtSlideshow = memo(({ artworks, user, autoPlay = true, interval = 4000 }:
       )}
 
       {/* Dots indicator */}
-      <div
-        className={cn("absolute flex z-30", isMobile ? "bottom-4 right-4 space-x-[2px]" : "bottom-6 right-6 space-x-1")}
-      >
+      <div className={cn("absolute flex z-30", isMobile ? "bottom-4 right-4 space-x-[2px]" : "bottom-6 right-6 space-x-1")}>
         {auctions.map((_, index) => (
           <button
             key={index}
