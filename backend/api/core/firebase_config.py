7c4d378f856d42179322ea3d84b85de6276bad6e
scyyythe <caneteangel187@gmail.com>
2025-09-07 14:03:26 +0800

    modidied firbease ocnfig

--- backend/api/core/firebase_config.py
+++ backend/api/core/firebase_config.py
@@ -13,7 +13,14 @@ def initialize_firebase():
     if not cred_path:
         raise ValueError("FIREBASE_CREDENTIALS is not set in .env")
 
+    # Default local path
     abs_path = os.path.join(os.getcwd(), cred_path.replace("/", os.sep))
+
+    # If running on Render, secret files are in /etc/secrets/<filename>
+    render_secret_path = os.path.join("/etc", cred_path)
+    if os.path.exists(render_secret_path):
+        abs_path = render_secret_path
+
     if not os.path.exists(abs_path):
         raise ValueError(f"Firebase credentials file not found: {abs_path}")
 
