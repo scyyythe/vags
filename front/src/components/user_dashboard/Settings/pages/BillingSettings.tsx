27dd4d4be252bb69a80a9b390dcff875b86c2f1b
JaiAnoba <kaeanver@gmail.com>
2025-09-17 16:54:11 +0800

    fixed tabs to not cause movements

--- front/src/components/user_dashboard/Settings/pages/BillingSettings.tsx
+++ front/src/components/user_dashboard/Settings/pages/BillingSettings.tsx
@@ -8,7 +8,7 @@ const BillingSettings = () => {
   return (
     <div className="w-full max-w-full mx-auto px-4">
       {/* Tabs */}
-      <div className="flex gap-8 mb-6 text-xs font-semibold  border-border">
+      <div className="flex gap-8 mb-6 text-xs font-semibold border-border">
         <button
           className={`pb-2 px-1 border-b-2 transition-colors duration-200 ${
             activeTab === "payment-accounts"
