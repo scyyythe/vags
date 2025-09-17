3ac10e9daddb08f547fc2aa395db2c105038d70f
JaiAnoba <kaeanver@gmail.com>
2025-09-17 16:50:21 +0800

    updated the table in transaction history

--- front/src/components/user_dashboard/Settings/pages/BillingSettings.tsx
+++ front/src/components/user_dashboard/Settings/pages/BillingSettings.tsx
@@ -3,22 +3,12 @@ import PaymentAccountsTab from "../components/tab/PaymentAccountsTab";
 import TransactionsTab from "../components/tab/TransactionTab";
 
 const BillingSettings = () => {
-  const [activeTab, setActiveTab] = useState<"transactions" | "payment-accounts">("transactions");
+  const [activeTab, setActiveTab] = useState<"transactions" | "payment-accounts">("payment-accounts");
 
   return (
     <div className="w-full max-w-full mx-auto px-4">
       {/* Tabs */}
       <div className="flex gap-8 mb-6 text-xs font-semibold  border-border">
-        <button
-          className={`pb-2 px-1 border-b-2 transition-colors duration-200 ${
-            activeTab === "transactions"
-              ? "text-red-800 border-red-800"
-              : "text-muted-foreground hover:text-foreground border-transparent"
-          }`}
-          onClick={() => setActiveTab("transactions")}
-        >
-          Transaction History
-        </button>
         <button
           className={`pb-2 px-1 border-b-2 transition-colors duration-200 ${
             activeTab === "payment-accounts"
@@ -28,6 +18,17 @@ const BillingSettings = () => {
           onClick={() => setActiveTab("payment-accounts")}
         >
           Payment Accounts
+        </button>
+
+        <button
+          className={`pb-2 px-1 border-b-2 transition-colors duration-200 ${
+            activeTab === "transactions"
+              ? "text-red-800 border-red-800"
+              : "text-muted-foreground hover:text-foreground border-transparent"
+          }`}
+          onClick={() => setActiveTab("transactions")}
+        >
+          Transaction History
         </button>
       </div>
 
