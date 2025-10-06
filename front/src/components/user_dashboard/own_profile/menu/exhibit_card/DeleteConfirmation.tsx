963dd730dd5290c73b6185ff19b35d9ef0d29655
JaiAnoba <kaeanver@gmail.com>
2025-10-06 19:02:15 +0800

    updated the DeleteConfirmation and DeletePopup

--- front/src/components/user_dashboard/own_profile/menu/exhibit_card/DeleteConfirmation.tsx
+++ front/src/components/user_dashboard/own_profile/menu/exhibit_card/DeleteConfirmation.tsx
@@ -14,20 +14,30 @@ const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50 overflow-hidden">
-      <div className="bg-white rounded-lg py-7 shadow-xl max-w-sm w-full text-center relative">
-        <h2 className="text-xs font-semibold text-gray-900 mb-2">You are about to delete this exhibit</h2>
-        <p className="text-[10px] text-gray-500 mb-6">This will permanently remove the exhibit from the dashboard.</p>
-        <div className="flex justify-center gap-16">
+    <div
+      className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50 overflow-hidden"
+      onClick={(e) => e.stopPropagation()}
+    >
+      <div
+        className="bg-white rounded-lg py-7 px-8 shadow-xl max-w-sm w-full text-center relative"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h2 className="text-xs font-semibold text-gray-900 mb-2">
+          You are about to delete this exhibit
+        </h2>
+        <p className="text-[10px] text-gray-500 mb-6">
+          This will permanently remove the exhibit from the dashboard.
+        </p>
+        <div className="flex justify-between gap-6">
           <button
             onClick={onCancel}
-            className="text-[10px] px-8 py-1 text-gray-600 hover:text-black border border-gray-500 hover:border-black rounded-full transition-colors duration-200"
+            className="w-full text-[10px] px-8 py-1 text-gray-600 hover:text-black border border-gray-500 hover:border-black rounded-full transition-colors duration-200"
           >
             Cancel
           </button>
           <button
             onClick={onConfirm}
-            className="bg-red-700 hover:bg-red-600 text-white text-[10px] px-8 py-1 rounded-full"
+            className="w-full bg-red-700 hover:bg-red-600 text-white text-[10px] px-8 py-1 rounded-full"
           >
             Delete
           </button>
