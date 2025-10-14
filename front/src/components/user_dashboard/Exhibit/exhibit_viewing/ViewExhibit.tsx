b82af6f1beeba2dbca0b56b57d1339813db22d43
JaiAnoba <kaeanver@gmail.com>
2025-10-14 17:47:57 +0800

    adjusted grid layout of related exhibits section

--- front/src/components/user_dashboard/Exhibit/exhibit_viewing/ViewExhibit.tsx
+++ front/src/components/user_dashboard/Exhibit/exhibit_viewing/ViewExhibit.tsx
@@ -459,7 +459,7 @@ const ExhibitViewing = () => {
               });
 
               return relatedExhibits.length > 0 ? (
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                   {relatedExhibits.map((card) => (
                     <ExhibitCard
                       key={card.id}
